cd7cfb75a589c34267dbd0876996c6120ad7c84d
Travis Bale <travis.bale@gmail.com>
2022-02-20 16:27:52 -0600

    Add some additional keymaps

--- nvim/config/keymaps.lua
+++ nvim/config/keymaps.lua
@@ -10,11 +10,16 @@ keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Move text up and down
+-- Move text up and down macos
 keymap("n", "∆", ":m .+1<CR>==", opts)
 keymap("n", "˚", ":m .-2<CR>==", opts)
 keymap("v", "∆", ":m '>+1<CR>gv=gv", opts)
 keymap("v", "˚", ":m '<-2<CR>gv=gv", opts)
+-- Move text up and down linux
+keymap("n", "<A-j>", ":m .+1<CR>==", opts)
+keymap("n", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
 
 -- Visual --
 -- Stay in indent mode
@@ -29,6 +34,12 @@ keymap("n", "<leader>fb", "<cmd>Telescope buffers<CR>", opts)
 keymap("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", opts)
 
 -- Nvimtree
-keymap("n", "<leader>e", ":NvimTreeFindFileToggle<CR>", opts)
-keymap("n", "<C-\\>", ":NvimTreeFindFileToggle<CR>", opts)
+keymap("n", "<leader>e", "<cmd>NvimTreeFindFileToggle<CR>", opts)
+keymap("n", "<C-\\>", "<cmd>NvimTreeFindFileToggle<CR>", opts)
+
+-- Jumplist
+keymap("n", "}", ':<C-u>execute "keepjumps norm! " . v:count1 . "}"<CR>', opts)
+keymap("n", "{", ':<C-u>execute "keepjumps norm! " . v:count1 . "{"<CR>', opts)
+
+-- Git
 
