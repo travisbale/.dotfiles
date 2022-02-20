9184434496a8971c385e6a43d0b083f6987c4e54
Travis Bale <travis.bale@gmail.com>
2022-02-20 16:27:10 -0600

    Change alpha dashboard to use git_files

--- nvim/config/alpha.lua
+++ nvim/config/alpha.lua
@@ -6,7 +6,7 @@ end
 
 local dashboard = require("alpha.themes.dashboard")
 dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+	dashboard.button("f", "  Find file", ":Telescope git_files <CR>"),
 	dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
 	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
 	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
