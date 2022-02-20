94896eba2e5bd9929f9f12501aea5447e53180dd
Travis Bale <travis.bale@gmail.com>
2022-02-20 16:59:22 -0600

    Format all lua files using stylua

--- nvim/config/alpha.lua
+++ nvim/config/alpha.lua
@@ -1,17 +1,16 @@
-
 local status_ok, alpha = pcall(require, "alpha")
 if not status_ok then
-	return
+    return
 end
 
 local dashboard = require("alpha.themes.dashboard")
 dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find file", ":Telescope git_files <CR>"),
-	dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+    dashboard.button("f", "  Find file", ":Telescope git_files <CR>"),
+    dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
+    dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+    dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+    dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+    dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
 
 alpha.setup(dashboard.opts)
