6956b4c97a42489600a6de7c0b746948cccf494e
Travis Bale <travis.bale@gmail.com>
2022-02-21 00:56:09 -0600

    Fix "format on save" autocommand

--- nvim/config/lsp/handlers.lua
+++ nvim/config/lsp/handlers.lua
@@ -85,11 +85,15 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-    if client.name == "tsserver" or client.name == "gopls" then
+    if client.name == "tsserver" or client.name == "gopls" or client.name == "sumneko_lua" then
         client.resolved_capabilities.document_formatting = false
     end
     lsp_keymaps(bufnr)
     lsp_highlight_document(client)
+
+    vim.api.nvim_command([[
+        autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()
+    ]])
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
