989426ac71d5e2e3057c059296b76ab89db936b6
Travis Bale <travis.bale@gmail.com>
2022-11-15 22:33:35 -0600

    Add treesitter-context plugin to neovim

--- nvim/config/plugins.lua
+++ nvim/config/plugins.lua
@@ -92,6 +92,7 @@ return packer.startup(function(use)
     -- Treesitter
     use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
     use("nvim-treesitter/playground")
+    use("romgrk/nvim-treesitter-context")
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
