7f422ab713566a1ee3e3d90679f8a7c9c08329f4
Travis Bale <travis.bale@gmail.com>
2023-02-21 17:40:28 -0600

    Display ignored and untracked files with telescope

--- nvim/config/telescope.lua
+++ nvim/config/telescope.lua
@@ -2,6 +2,10 @@ require("telescope").setup({
     pickers = {
         find_files = {
             hidden = true,
+            no_ignore = true,
+        },
+        git_files = {
+            show_untracked = true,
         },
     },
 })
