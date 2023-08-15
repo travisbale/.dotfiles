cbf8e061b1116309cfcb64c04403e2939ed6039f
Travis Bale <travis.bale@gmail.com>
2023-08-15 11:06:04 -0500

    Fix outdated configuration

--- nvim/config/nvim-tree.lua
+++ nvim/config/nvim-tree.lua
@@ -1,5 +1,3 @@
-local tree_cb = require("nvim-tree.config").nvim_tree_callback
-
 require("nvim-tree").setup({
     diagnostics = {
         enable = true,
@@ -8,16 +6,6 @@ require("nvim-tree").setup({
             info = "",
             warning = "",
             error = "",
-        },
-    },
-    view = {
-        mappings = {
-            list = {
-                { key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
-                { key = { "<Right>", "<CR>", "o" }, cb = tree_cb("edit") },
-                { key = "h", cb = tree_cb("close_node") },
-                { key = "<Left>", cb = tree_cb("close_node") },
-            },
         },
     },
 })
