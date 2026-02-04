bd79f5a3b9a59afa87ee8749ddbb07b5d3ef6ad2
Travis Bale <travis.bale@gmail.com>
2026-02-04 15:25:15 -0600

    Update treesitter config

--- nvim/plugins/treesitter.lua
+++ nvim/plugins/treesitter.lua
@@ -1,45 +1,19 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
 
-    init = function(plugin)
-        -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-        -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-        -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-        -- Luckily, the only things that those plugins need are the custom queries, which we make available
-        -- during startup.
-        require("lazy.core.loader").add_to_rtp(plugin)
-        require("nvim-treesitter.query_predicates")
-    end,
+    config = function()
+        -- Install all parsers
+        require("nvim-treesitter").install("all")
 
-    opts_extend = { "ensure_installed" },
-
-    opts = {
-        ensure_installed = "all",
-
-        -- Install languages synchronously (only applied to `ensure_installed`)
-        sync_install = false,
-
-        -- List of parsers to ignore installing
-        ignore_install = {},
-
-        highlight = {
-            -- `false` will disable the whole extension
-            enable = true,
-
-            -- list of language that will be disabled
-            disable = {},
-
-            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-            -- Using this option may slow down your editor, and you may see some duplicate highlights.
-            -- Instead of true it can also be a list of languages
-            additional_vim_regex_highlighting = false,
-        },
-    },
-
-    ---@param opts TSConfig
-    config = function(_, opts)
-        require("nvim-treesitter.configs").setup(opts)
+        -- Enable treesitter highlighting globally
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "*",
+            callback = function()
+                pcall(vim.treesitter.start)
+            end,
+        })
     end,
 }
