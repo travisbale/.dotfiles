45b71b9041cf3d9577f201b12eeceee638e824ee
Travis Bale <travis.bale@gmail.com>
2026-04-29 13:19:38 -0500

    Update treesitter installation process

--- nvim/plugins/treesitter.lua
+++ nvim/plugins/treesitter.lua
@@ -1,32 +1,36 @@
+local parsers = {
+    -- Shell / scripting
+    "bash",
+    -- Systems
+    "c", "cpp", "rust", "asm",
+    -- Web
+    "css", "html", "javascript", "jsdoc", "tsx", "typescript", "vue",
+    -- Compiled
+    "go", "gomod", "gosum", "java", "kotlin",
+    -- Dynamic
+    "lua", "python", "ruby", "php",
+    -- Config / data
+    "json", "yaml", "toml", "xml", "sql",
+    -- Infra / build
+    "dockerfile", "make", "cmake", "terraform", "hcl",
+    -- Docs
+    "markdown", "markdown_inline", "vim", "vimdoc",
+    -- Git
+    "diff", "gitcommit", "gitignore", "gitattributes", "git_config",
+    -- Misc
+    "proto", "regex", "comment",
+}
+
 return {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
     lazy = false,
-    build = ":TSUpdate",
 
     config = function()
-        require("nvim-treesitter").install({
-            -- Shell / scripting
-            "bash",
-            -- Systems
-            "c", "cpp", "rust", "asm",
-            -- Web
-            "css", "html", "javascript", "jsdoc", "tsx", "typescript", "vue",
-            -- Compiled
-            "go", "gomod", "gosum", "java", "kotlin",
-            -- Dynamic
-            "lua", "python", "ruby", "php",
-            -- Config / data
-            "json", "yaml", "toml", "xml", "sql",
-            -- Infra / build
-            "dockerfile", "make", "cmake", "terraform", "hcl",
-            -- Docs
-            "markdown", "markdown_inline", "vim", "vimdoc",
-            -- Git
-            "diff", "gitcommit", "gitignore", "gitattributes", "git_config",
-            -- Misc
-            "proto", "regex", "comment",
-        })
+        -- max_jobs = 1 installs parsers one at a time so startup doesn't
+        -- peg CPU on resource-constrained machines. Already-installed
+        -- parsers are a no-op, so this is only slow on first run.
+        require("nvim-treesitter").install(parsers, { max_jobs = 1 })
 
         -- Enable treesitter highlighting globally
         vim.api.nvim_create_autocmd("FileType", {
